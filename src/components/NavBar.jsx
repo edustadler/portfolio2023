68e1990e8bf7becf441d5c21fcd0132ab42d2452
Edu Stadler <32523518+edustadler@users.noreply.github.com>
2023-08-04 16:56:27 -0300

    fixes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,10 +10,10 @@ export default function NavBar() {
     return (<>
         <Section sx={{ position: "absolute", padding: ".9375rem 0" }} height="auto" bgColor="transparent">
             <Container sx={{ display: "flex", justifyContent: "end", alignItems: "center", gap: ".625rem" }} maxWidth="xl">
-                <Link href="#" style={{ zIndex: '999' }}>
+                <Link href="https://github.com/edustadler/" target='blank' style={{ zIndex: '999' }}>
                     <GitHub />
                 </Link>
-                <Link href="#" style={{ zIndex: '999' }}>
+                <Link href="https://www.linkedin.com/in/edu-stadler/" target='blank' style={{ zIndex: '999' }}>
                     <LinkedIn />
                 </Link>
             </Container>
